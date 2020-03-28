885ea7327dc3494ec846bc0d29bcf8c7480294ce
VANSH RISHI <43612972+vanshrishi@users.noreply.github.com>
2020-03-28 13:24:13 +0530

    Update TreeNode.java

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -1,3 +1,4 @@
+
 /*
  *  Created by IntelliJ IDEA.
  *  User: Vaibhav
@@ -7,4 +8,37 @@
 package problem1.node;
 
 public class TreeNode {
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
 }
