962c107faec8b9d48b7bf244439f3e9047d0a40a
VANSH RISHI <43612972+vanshrishi@users.noreply.github.com>
2020-03-28 13:23:11 +0530

    Update MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,3 +1,4 @@
+
 /*
  *  Created by IntelliJ IDEA.
  *  User: Vaibhav
@@ -7,5 +8,22 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree binarySearchTree = new MyBinarySearchTree();
+        binarySearchTree.insert(15);
+        binarySearchTree.insert(10);
+        binarySearchTree.insert(50);
+        binarySearchTree.insert(55);
+        binarySearchTree.insert(60);
+
+        System.out.println("Left children of the binary search tree: ");
+        binarySearchTree.traverse();
+        System.out.println("No.of Parents not having their left child are:");
+        System.out.println(binarySearchTree.getCountRight());
+
+    }
 }
+
