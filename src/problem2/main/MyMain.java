df61c3271cea93a219360094eec5aebae54d314f
VANSH RISHI <43612972+vanshrishi@users.noreply.github.com>
2020-03-31 20:50:57 +0530

    Update MyMain.java

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -1,9 +1,4 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:15 PM
- */
+
 package problem2.main;
 
 import problem2.main.binarySearchTree.MyBinarySearchTree;
